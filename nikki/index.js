c72eae963c19f4bde8103f9f5b78d1441a2e7454
Valeriy Yurchenko <leon61@ukr.net>
2018-09-05 09:04:41 +0000

    0.6

---
+++ nikki/index.js
@@ -0,0 +1,5 @@
+$('#exampleModalCenter').modal('show');
+
+(function() {
+const noWay = "color: lime;" + "font-size: 60px;" + "font-weight: bold;" + "text-shadow: 1px 1px 5px rgb(0, 0, 0);" + "filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);"; setTimeout(console.log.bind(console, "%cCreated by LeoN61", noWay), 0);
+})();
